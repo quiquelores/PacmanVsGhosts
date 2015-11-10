6b243fddabe9977d7469ca139dc6d1a0f62711a2
quiquelores <quique.lores@gmail.com>
2015-11-10 18:23:46 -0500

    Hillclimber

--- src/pacman/entries/pacman/PacmanHillclimber.java
+++ src/pacman/entries/pacman/PacmanHillclimber.java
@@ -1,5 +1,6 @@
 package pacman.entries.pacman;
 
+import pacman.Executor;
 import pacman.controllers.Controller;
 import pacman.controllers.examples.StarterGhosts;
 import pacman.game.Constants.DM;
@@ -19,14 +20,13 @@ public class PacmanHillclimber extends Controller<MOVE>
 	public MOVE getMove(Game game, long timeDue)
 	{
 		int currBestEval = Integer.MIN_VALUE;
-		
+
 		MOVE[] possibleMoves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
-		
 		for(MOVE move : possibleMoves){
-			
+
 			Game gameCopy = game.copy();
-			gameCopy.advanceGame(move, new StarterGhosts().getMove());
-			
+			gameCopy.advanceGame(move, Executor.ghostAI.getMove());
+
 			if(evalGameState(gameCopy)>currBestEval){
 				myMove = move;
 				currBestEval = evalGameState(gameCopy);
@@ -36,39 +36,20 @@ public class PacmanHillclimber extends Controller<MOVE>
 	}
 	
 	private int evalGameState(Game game){
-		int score = 0;
 		
-		//being close to ghosts will lead to a lower score. eating a ghost will lead to a higher score
+		if(game.wasPacManEaten()) return Integer.MIN_VALUE;
+
+		int score = game.getScore();
+
+		//having edible ghosts will lead to a higher score. being close to them will lead to a higher score if edible, and to a lower score if they're not edible.
 		for(GHOST ghost: GHOST.values()){
-			if(game.wasGhostEaten(ghost)){
-				score += 1000; //random value. we should play with it
-			} else if(game.isGhostEdible(ghost)){
-				score += 500;
+			if(game.isGhostEdible(ghost)){
+				score += 50;
 				score -= game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghost), DM.PATH);
 			} else {
 				score += game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghost), DM.PATH);
 			}
 		}
-		
-		//being far close to pills will lead to a higher score
-		
-		//get all active pills
-		int[] activePills=game.getActivePillsIndices();
-
-		//get all active power pills
-		int[] activePowerPills=game.getActivePowerPillsIndices();
-
-		//create a target array that includes all ACTIVE pills and power pills
-		int[] targetNodeIndices=new int[activePills.length+activePowerPills.length];
-
-		for(int i=0;i<activePills.length;i++)
-			targetNodeIndices[i]=activePills[i];
-
-		for(int i=0;i<activePowerPills.length;i++)
-			targetNodeIndices[activePills.length+i]=activePowerPills[i];
-		
-		score -= 100*game.getDistance(game.getPacmanCurrentNodeIndex(),game.getClosestNodeIndexFromNodeIndex(game.getPacmanCurrentNodeIndex(),targetNodeIndices,DM.PATH),DM.PATH);
-		
 		return score;
 	}
 }
