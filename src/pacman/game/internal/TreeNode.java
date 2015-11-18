10f532265596ac64634936e19e4a805e9e341846
quiquelores <quique.lores@gmail.com>
2015-11-18 17:17:19 -0500

    TreeNode class

--- src/pacman/game/internal/TreeNode.java
+++ src/pacman/game/internal/TreeNode.java
@@ -6,37 +6,50 @@ import pacman.Executor;
 import pacman.game.Game;
 import pacman.game.Constants.MOVE;
 
+	//Class adapted from AStar.java
 public class TreeNode implements Comparable<TreeNode>
-{
-	public Game gameState;
-	public int index;
-	public EnumMap<MOVE, TreeNode> neighbors = new EnumMap<MOVE, TreeNode>(MOVE.class);
-	public double g = 0;
-	public double h = 0;
-	public TreeNode parent = null;
-	public MOVE reachedBy = null;
+	{
+		public Game gameState;
+		public int index;
+		public EnumMap<MOVE, TreeNode> neighbors = new EnumMap<MOVE, TreeNode>(MOVE.class);
+		public double g = 0;
+		public double h = 0;
+		public TreeNode parent = null;
+		public MOVE reachedBy = null;
 
-    public TreeNode(int index)
-    {
-        this.index=index;
-    }
-    
-    public TreeNode(Game game){
-    	this.gameState = game;
-    	this.index = game.getPacmanCurrentNodeIndex();
-    	this.reachedBy = game.getPacmanLastMoveMade();    	
-    }
-    public TreeNode evolve(MOVE move){
-    	Game game = this.gameState.copy();
-    	game.advanceGame(move, Executor.ghostAI.getMove());
-    	TreeNode next = new TreeNode(game);
-    	next.parent = this;
-    	return next;
-    }
-
-	public int compareTo(TreeNode t) {
-		if(StateEvaluator.evalGameState(this.gameState) - StateEvaluator.evalGameState(t.gameState) > 0) return -1;
-		
-		return 1;
+	    public TreeNode(int index)
+	    {
+	        this.index=index;
+	    }
+	    public TreeNode(Game game){
+	    	this.gameState = game;
+	    	this.index = game.getPacmanCurrentNodeIndex();
+	    	this.reachedBy = game.getPacmanLastMoveMade();    	
+	    }
+	    public TreeNode evolve(MOVE move){
+	    	Game game = this.gameState.copy();
+	    	game.advanceGame(move, Executor.ghostAI.getMove());
+	    	TreeNode next = new TreeNode(game);
+	    	return next;
+	    }
+	    
+	    public double f() {
+	    	return g + h;
+	    }
+	    
+	    public boolean isEqual(TreeNode other)
+	    {
+	        return index == other.index;
+	    }
+	    
+	    public int compareTo(TreeNode other)
+	    {
+			if (f() < other.f()) {
+				return -1;
+			}
+			else if (f() > other.f()) {
+				return 1;
+			}
+			return 0;
+	    }
 	}
-}
